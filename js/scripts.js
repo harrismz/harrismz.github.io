e6ca9a7aac4624acedb83f91abd37f8c8264f04a
Harris <harris.git21@gmail.com>
2023-08-04 21:17:23 +0700

    fix! : Change template Resume to My resume

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Start Bootstrap - Zakiso v1.0.1 (https://startbootstrap.com/template-overviews/Zakiso)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-Zakiso/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
